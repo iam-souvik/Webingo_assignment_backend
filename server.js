56be3dcf70cfdb49beea8facb955d00fe666cfc8
iam-souvik <souvikpatramax@gmail.com>
2023-08-27 10:36:08 +0530

    final_all-error-fixed

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const connectDatabase = require("./connect/db")
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const authCheck = require('./middleware/auth.middleware');
 const managerRoutes = require('./routes/manager');
-// const adminRoutes = require('./routes/admin');
+
 
 const app = express();
+require("dotenv").config()
 
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-const uri = "mongodb://0.0.0.0:27017/webingo_assignment";
-mongoose.connect(uri, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('✅Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
+
 
 // Use routes
 app.use('/auth', authRoutes);
@@ -40,7 +31,17 @@ app.use("*", (req, res) => {
   res.send({message: "Wrong end-point"})
 })
 
-const PORT = process.env.PORT || 8000;
+
+
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-  console.log(` ✅ Server is running on port ${PORT}`);
-});
+    console.log(` ✅ Server is running on port ${PORT}`)
+    try {
+        connectDatabase()
+        console.log("Db Connected  ✅")
+
+    } catch (err) {
+        console.log(err.message)
+    }
+
+})
